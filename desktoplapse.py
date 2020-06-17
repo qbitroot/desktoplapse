1442d2b7e5023e84b0ccfe9f6b45211a2c5504f9
Vlad <66268477+gangural@users.noreply.github.com>
2020-06-17 22:02:56 +0300

    Update desktoplapse.py

--- desktoplapse.py
+++ desktoplapse.py
@@ -14,7 +14,7 @@ parser.add_argument('-s', "--speed", type=float, default=5.0, help="speed up fac
 parser.add_argument('-o', "--output", help="location of output video file, default is TIMESTAMP.mp4")
 parser.add_argument('-q', "--quality", type=int, default=80,
 					help="JPEG quality from 0 to 100 of captured frames (default: %(default)s)")
-parser.add_argument('-r', "--reduce", type=float, default=1.0, help="image reduce factor (default: %(default)s)")
+parser.add_argument('-r', "--reduce", type=float, default=1.0, help="image size reduce factor (default: %(default)s)")
 parser.add_argument("--frame-folder", default='cap',
 					help="path to folder where captured frames are stored (default: %(default)s)")
 parser.add_argument("--preserve-frames", action='store_true', help="do not delete captured frames")
